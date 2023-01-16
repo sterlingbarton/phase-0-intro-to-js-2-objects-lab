407f513abc71f64ac914bccf4bbf64e6bf0b96cc
Sterling <sterlingbarton81@gmail.com>
2023-01-16 12:36:57 -0600

    commit tree 641ef7e26edfed4cf3cd5d3713e536c10d529b23

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Bob',
+    streetAddress: '123 Northshore Drive',
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+
+}
